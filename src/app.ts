74aa26422885a53f975e52af8e1e0a23e6a17231
luisHenrickk <rick.ufms@gmail.com>
2023-04-03 15:14:32 -0400

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import { json } from 'body-parser';
+
+export const app = express();
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(json());
+
+app.use((err: Error, req: Request, res: Response) => {
+  res.status(500).json({ message: err.message });
+});
