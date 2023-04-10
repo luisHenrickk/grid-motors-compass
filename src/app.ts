e86f4657c2e46b9fe30727aa83f01b5622b17d50
luisHenrickk <rick.ufms@gmail.com>
2023-04-10 19:18:49 -0400

    feat/build
    feat: swagger implemented.
    build: added swagger libraries.

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,14 @@ import { userRouter } from './routes/userRouter';
 import { errorMiddleware } from './middlewares/error';
 import { authenticateRouter } from './routes/authenticateRouter';
 import { reserveRouter } from './routes/reserveRouter';
+import swaggerSetup from './swagger';
 
 export const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+swaggerSetup(app);
 
 app.use('/api/v1/car', carRouter);
 app.use('/api/v1/user', userRouter);
---
+++ src/swagger.ts
@@ -0,0 +1,9 @@
+import { Express } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+export default function setupSwagger(app: Express): void {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
